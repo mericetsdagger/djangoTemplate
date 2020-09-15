c4340c9bdd74c3b48b035c9ad3d2b804cd44b919
mericetsdagger <papaj.jacek@gmail.com>
2020-09-15 20:38:08 +0200

    dopisanie pól

--- privileges/admin.py
+++ privileges/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import FishDist, FishAsso, PrvType
 # Register your models here.
+admin.site.register(FishDist)
+admin.site.register(FishAsso)
+admin.site.register(PrvType)
--- privileges/models.py
+++ privileges/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class PrvType(models.Model):
     fishingDistrictId = models.ForeignKey('FishDist', on_delete=models.DO_NOTHING)
+    description = models.CharField(max_length=1000, default="nie wprowadzono opisu")
+    price = models.DecimalField(max_digits=10, decimal_places=2, default="0.00")
     #szczegóły dograć z Jarkiem
     class Meta:
         db_table = 'prv_type'
@@ -21,6 +23,7 @@ class FishDist(models.Model):
 class Prvs(models.Model):
     profileId = models.ForeignKey('userProfile.UserProfile', on_delete=models.CASCADE)
     prvTypeId = models.ForeignKey('PrvType', on_delete=models.DO_NOTHING)
+
     #szczegoly dogadac z Jarkiem
     class Meta:
         db_table = 'prvs'
