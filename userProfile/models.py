269526e7953f9159f16ab39cb42c0fed55628efb
mericetsdagger <papaj.jacek@gmail.com>
2020-09-18 18:34:16 +0200

    próba profilu

--- userProfile/models.py
+++ userProfile/models.py
@@ -1,12 +1,25 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from privileges.models import Prvs, PrvType, FishAsso, FishDist
 
 class UserProfile(models.Model):
-    profileType = models.IntegerField()
-    userId = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    assoRelated = models.ForeignKey('privileges.FishAsso', on_delete=models.DO_NOTHING, default = 1)
+    userId_id = models.OneToOneField(User, on_delete=models.CASCADE, related_name='+', unique=True)
+    profileType = models.IntegerField(default=1, null=True)
+    #userId = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    assoRelated = models.ForeignKey('privileges.FishAsso', on_delete=models.DO_NOTHING, default = 1, null=True)
+
+    def __str__(selfself):
+        return self.user.username
 
     class Meta:
         db_table = 'userprofile'
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
 
