7e9d2fa61bf14e691b84855bef08e6b3366aa083
mericetsdagger <papaj.jacek@gmail.com>
2020-09-18 20:52:56 +0200

    zapis profilu

--- userProfile/migrations/0001_initial.py
+++ userProfile/migrations/0001_initial.py
@@ -18,8 +18,9 @@ class Migration(migrations.Migration):
             name='UserProfile',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('profileType', models.IntegerField()),
-                ('userId', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                ('profileType', models.IntegerField(default=1)),
+                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='+', to=settings.AUTH_USER_MODEL)),
+                ('fish_asso', models.ForeignKey(null=True, on_delete=django.db.models.deletion.DO_NOTHING, to='privileges.FishAsso')),
             ],
         ),
     ]
