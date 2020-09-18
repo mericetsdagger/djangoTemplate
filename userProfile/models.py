42345ea231027f4dfa0532b3b5d44acc0b5587e7
mericetsdagger <papaj.jacek@gmail.com>
2020-09-18 19:46:05 +0200

    działa propagacja usera do profilu

--- userProfile/models.py
+++ userProfile/models.py
@@ -6,10 +6,9 @@ from django.dispatch import receiver
 from privileges.models import Prvs, PrvType, FishAsso, FishDist
 
 class UserProfile(models.Model):
-    userId_id = models.OneToOneField(User, on_delete=models.CASCADE, related_name='+', unique=True)
-    profileType = models.IntegerField(default=1, null=True)
-    #userId = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    assoRelated = models.ForeignKey('privileges.FishAsso', on_delete=models.DO_NOTHING, default = 1, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='+', unique=True)
+    profileType = models.IntegerField(default=1)
+    fish_asso = models.ForeignKey('privileges.FishAsso', on_delete=models.DO_NOTHING, null=True)
 
     def __str__(selfself):
         return self.user.username
