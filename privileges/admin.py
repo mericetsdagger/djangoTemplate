ab0f7bfd46f0e2a4902b1d5b7d3b1d24a5a23864
mericetsdagger <papaj.jacek@gmail.com>
2020-09-19 13:40:40 +0200

    dodane moje licencje, nie dziala

--- privileges/admin.py
+++ privileges/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import FishDist, FishAsso, PrvType
+from .models import FishDist, FishAsso, PrvType, Prvs
 # Register your models here.
 admin.site.register(FishDist)
 admin.site.register(FishAsso)
 admin.site.register(PrvType)
+admin.site.register(Prvs)
--- userProfile/views.py
+++ userProfile/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from .models import UserProfile
 from django.views import generic
+from privileges.models import Prvs
 
 class ProfileView(generic.ListView):
     template_name = 'profile/profileMain.html'
     model = UserProfile
+
+    def my_prvs(request):
+        obj = Prvs.objects.get(id=user_id)
+        context = {
+            'object': obj
+        }
+        return render(request, template_name, context)
+
